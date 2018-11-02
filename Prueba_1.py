118674e94e93f7c4fe0ca23cabf842f456bcb2be
angelo <angelozambrano@gmail.com>
2018-11-01 22:11:37 -0500

    mi primer commit

---
+++ Prueba_1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#  # esto es una prueba MARKDOWN
+#  ## ESTO ES UN SUBTITULO
+#  ESTO ES UN TEXTO
+#  
+#  *ESTO ES UN TEXTO EN CURSIVA
+#  
+#  **ESTO ES NEGITA
+# 
+# 
+
+# In[1]:
+
+
+print ("HOLA MUNDO")
+
+
+# In[ ]:
+
+
+
+
+
+# In[4]:
+
+
+print ("print es una instruccion que muestra texto en pantalla") 
+
